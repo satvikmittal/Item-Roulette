b7d6f8c80eb362c1963fb60f0806c4e84b51ce89
satvikmittal <66307979+satvikmittal@users.noreply.github.com>
2020-12-24 20:35:09 +0530

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -17,6 +17,12 @@ player1inv = ["Car", "House", "Mobile", "PC"]
 
 player2inv = ["$100,000", "House", ""]
 
+global Player1
+global Player2
+global player1chosenItem
+global player2chosenItem
+
+player = 1
 
 def instructions():
     print("HI, Welcome to Roulette!!")
@@ -30,16 +36,18 @@ def printItems():
         print(i)
 
 def printPlayer1inv():
-    print("Player 1, These are the items you have")
+    print(Player1 + ", These are the items you have")
     for i in player1inv:
         print(i)
 
 def printPlayer2inv():
-    print("Player 2 These are the items you have")
+    print(Player2 + ", These are the items you have")
     for i in player2inv:
         print(i)
 
 def enterName():
+    global Player1
+    global Player2
     Player1 = str(input("Player 1 enter your name: "))
     Player2 = str(input("Player 2 enter your name: "))
 
@@ -58,6 +66,27 @@ def enterAnyKey(doQuit):
         else:
             pass
 
+def player1bet():
+    global player1chosenItem
+    player1chosenItem = ""
+    itemChosen = input(Player1 + ", Choose an item you'll give to " + Player2 + " if you lose: ")
+    while player1chosenItem == "":
+        if itemChosen in player1inv:
+            player1chosenItem = itemChosen
+        else:
+            itemChosen = input("You don't have that item choose another: ")
+
+def player2bet():
+    global player2chosenItem
+    player2chosenItem = ""
+    itemChosen = input(Player2 + ", Choose an item you'll give to " + Player1 + " if you lose: ")
+
+    while player2chosenItem == "":
+        if itemChosen in player2inv:
+            player2chosenItem = itemChosen
+        else:
+            itemChosen = input("You don't have that item choose another: ")
+
 
 instructions()
 enterAnyKey(False)
@@ -65,6 +94,6 @@ enterName()
 printPlayer1inv()
 printPlayer2inv()
 enterAnyKey(True)
-
-
-
+player1bet()
+player2bet()
+enterAnyKey(False)
